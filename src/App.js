cf6261cc721ea3adbddbbf877d91f8abd85b738d
luvgameon <luvgameon@gmail.com>
2023-02-26 19:57:29 +0530

    Delete Movie

--- src/App.js
+++ src/App.js
@@ -1,104 +1,134 @@
 import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 import AddMovie from "./components/AddMovie";
 
-
 function App() {
   const [Movies, setMovies] = useState([]);
- const [Isloading, setIsloading] = useState(false);
- const [Iserror, setIserror] = useState(null);
+  const [Isloading, setIsloading] = useState(false);
+  const [Iserror, setIserror] = useState(null);
 
-
-  const FetchMovie=useCallback(async ()=>{
-  
-    
+  const FetchMovie = useCallback(async () => {
     setIserror(null);
     setIsloading(true);
+     // <----------------------------------------FetchMovie start-------------------------------->
     try {
-      
-      const response = await fetch("https://swapi.dev/api/films");
-      if(!response.ok){
-      throw new Error("SomeThing Went Wrong!!! ...Retrying");
+      const response = await fetch(
+        "https://react-http-6410a-default-rtdb.firebaseio.com/movie.jso"
+      );
+      if (!response.ok) {
+        throw new Error("SomeThing Went Wrong!!! ...Retrying");
       }
       const data = await response.json();
-  
-      const trasformMovie = data.results.map((i) => {
-        return {
-          id: i.episode_id,
-          title: i.title,
-          openingText: i.opening_crawl,
-          releaseDate: i.release_date,
-        };
-      });
+
+      const trasformMovie = [];
+      for (const key in data) {
+        trasformMovie.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      console.log(trasformMovie);
       setMovies(trasformMovie);
-      
-      
     } catch (error) {
       setIserror(error.message);
-      
     }
     setIsloading(false);
-   
-  },[]);
-  useEffect(()=>{
+  }, []);
+
+
+
+   // <----------------------------------------Automatic Fetching-------------------------------->
+
+  useEffect(() => {
     FetchMovie();
-   },[FetchMovie]);
+  }, [FetchMovie]);
 
-
-  function stop(){
   
-console.log('running')
-   clearInterval();
-  }
-
-  const AddMoviefunction=(movie)=>{
-    console.log(movie);
-  }
-  let content=<h4>No Movies Found Try Fetching some Movies</h4>
+  // <----------------------------------------Add Movie Function startTransition-------------------------------->
   
-  if(Iserror)
-  {
+  const AddMoviefunction = async (movie) => {
+    const respose = await fetch(
+      "https://react-http-6410a-default-rtdb.firebaseio.com/movie.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+      );
+      const data = await respose.json();
+      console.log(data);
+    };
     
-    setInterval(FetchMovie, 5000)
+    // <----------------------------------------Delete Movie Function -------------------------------->
+    
+    const deleteMovie = (id) => {
+    axios.delete(
+      `https://react-http-6410a-default-rtdb.firebaseio.com/movie/${id}.json`
+      );
+      console.log("delete done", id);
+    };
 
-    
-    
-    
-  
-      content=<><p>{Iserror} </p>
-     <button onClick={stop}>Stop Retrying</button>
-      </>
+   // <----------------------------------------Delete Movie Function End-------------------------------->
+
+   let intervalId;
    
-      
+   // <----------------------------------------Retry function-------------------------------->
+   useEffect(() => {
+     if (Iserror) {
+       intervalId = setInterval(FetchMovie, 3000);
+       
+       return () => {
+         clearInterval(intervalId);
+        };
+      }
+    });
+    
+    function stop() {
+      console.log("running");
+      clearInterval(intervalId);
+      setIserror(false);
+    }
+     // <----------------------------------------Content Changes-------------------------------->
+
+
+    let content = <h4>No Movies Found Try Fetching some Movies</h4>;
+    
+  if (Iserror) {
+    content = (
+      <>
+        <p>{Iserror} </p>
+        <button onClick={stop}>Stop Retrying</button>
+      </>
+    );
   }
-  
-  if(Isloading)
-  {
-    content=<p>Loading...</p>;
+
+  if (Isloading) {
+    content = <p>Loading...</p>;
   }
-  if(Movies.length>0)
-  {
-    content=<MoviesList movies={Movies} />
+  if (Movies.length > 0) {
+    content = <MoviesList movies={Movies} deleteMovie={deleteMovie} />;
   }
-      
+
+
 
   
 
   return (
     <React.Fragment>
       <section>
-       <AddMovie addmovie={AddMoviefunction}/>
+        <AddMovie addmovie={AddMoviefunction} />
       </section>
       <section>
         <button onClick={FetchMovie}>Fetch Movies</button>
-      
       </section>
-      <section>
-       {content}
-       
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import classes from './Movie.module.css';
+function Movie(props){
+  
+  const DeleteMovie=()=>{
+    console.log('inside movie ',props.id);
+    
+    props.deleteMovie(props.id);
 
-const Movie = (props) => {
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>Release Date: {props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button style={{background:'grey'}} onClick={DeleteMovie}>Delete Movie</button>
     </li>
   );
 };
